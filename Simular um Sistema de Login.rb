b650eb6193802f03b6b9c7a1d1daff80b41d321b
Marta Aparício <36605729+martasaparicio@users.noreply.github.com>
2021-06-27 17:28:13 +0100

    Update and rename Simular um Sistema de Login to Simular um Sistema de Login.rb

--- Simular um Sistema de Login.rb
+++ Simular um Sistema de Login.rb
@@ -1,20 +1,27 @@
+#1
 puts "Insert Username:"
 new_username = gets.chomp
 
+#2
 puts "Insert Password:"
 new_password = gets.chomp
 
+#3
 puts "Insert Age:"
 new_age = gets.chomp.to_i
 
+#4
 puts "Get Username:"
 get_username = gets.chomp
 
+#5
 puts "Get Password"
 get_password = gets.chomp
 
+#6
 if new_age >= 18 && get_username == new_username && get_password == new_password
   puts "Successful Login :)"
+ #7
 else
   puts "Unsuccessful Login :("
 end
